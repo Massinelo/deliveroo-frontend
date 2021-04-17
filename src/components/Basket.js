33a8830d49bca0def6607e012bb2c7a614085f26
Massinelo <massi.ait@gmail.com>
2021-04-17 02:44:21 +0200

    basket v2

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { AiOutlinePlusCircle, AiOutlineMinusCircle } from 'react-icons/ai';
 
 const Basket = ({ products, setProducts }) => {
-  let total = 0;
+  let sousTotal = 0;
   for (let i = 0; i < products.length; i++) {
-    total = total + products[i].price * products[i].quantity;
+    sousTotal = sousTotal + products[i].price * products[i].quantity;
   }
+
+  let delivery = 2.5;
+
+  let total = sousTotal + delivery;
 
   const empty = products.length === 0;
 
@@ -18,40 +22,61 @@ const Basket = ({ products, setProducts }) => {
         {empty ? (
           <div className="empty">Votre panier est vide</div>
         ) : (
-          <div>
+          <div className="products">
             {products.map((product, index) => {
               return (
-                <div className="product" key={index}>
-                  <div>
-                    <AiOutlineMinusCircle
-                      onClick={() => {
-                        //Crée une copie du state
-                        const newProducts = [...products];
-                        //Modifier la copie (supprimer le produit si égale à 1 ou décrementer la quantité)
-                        if (newProducts[index].quantity === 1) {
-                          newProducts.splice(index, 1);
-                        } else {
-                          newProducts[index].quantity--;
-                        }
-                        //Mettre à jour le state
-                        setProducts(newProducts);
-                      }}
-                    />
-                    {product.quantity}
-                    <AiOutlinePlusCircle
-                      onClick={() => {
-                        const newProducts = [...products];
-                        newProducts[index].quantity++;
-                        setProducts(newProducts);
-                      }}
-                    />{' '}
-                    {product.title} - {product.price}
+                <div key={index}>
+                  <div className="product">
+                    <span className="quantityBox">
+                      <AiOutlineMinusCircle
+                        className="buttons"
+                        onClick={() => {
+                          //Crée une copie du state
+                          const newProducts = [...products];
+                          //Modifier la copie (supprimer le produit si égale à 1 ou décrementer la quantité)
+                          if (newProducts[index].quantity === 1) {
+                            newProducts.splice(index, 1);
+                          } else {
+                            newProducts[index].quantity--;
+                          }
+                          //Mettre à jour le state
+                          setProducts(newProducts);
+                        }}
+                      />
+                      <span className="quantity">{product.quantity}</span>
+                      <AiOutlinePlusCircle
+                        className="buttons"
+                        onClick={() => {
+                          const newProducts = [...products];
+                          newProducts[index].quantity++;
+                          setProducts(newProducts);
+                        }}
+                      />
+                    </span>
+                    <span className="productTitle">{product.title}</span>
+                    <span>{product.price} €</span>
                   </div>
                 </div>
               );
             })}
             {/* Permet d'arrondir le total 2 chiffres après la virgules */}
-            total : {total.toFixed(2)}
+          </div>
+        )}
+        {empty ? null : (
+          <div>
+            {' '}
+            <div className="sousTotal">
+              <div>
+                {' '}
+                <span>Sous-total :</span> <span>{sousTotal.toFixed(2)} € </span>
+              </div>
+              <div>
+                <span>Frais de livraison :</span> <span>{delivery} €</span>{' '}
+              </div>
+            </div>
+            <div className="total">
+              <span>Total :</span> <span>{total.toFixed(2)} €</span>
+            </div>
           </div>
         )}
       </div>
